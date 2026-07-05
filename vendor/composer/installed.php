72374579893eb86d25aa1a8cfb8687d55f23d4dd
telarambar-dot <telarambar@gmail.com>
2026-07-05 19:42:28 +0330

    4

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rubikabot/rubikabot',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bb7970ccaa4109e86827c89042b3b3e0403b751a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rubikabot/rubikabot' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bb7970ccaa4109e86827c89042b3b3e0403b751a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
